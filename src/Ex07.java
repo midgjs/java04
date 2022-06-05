d2b0094753224114864c171854b2bb5052930b9a
midgjs <sjqksk.10@gmail.com>
2022-06-05 22:06:43 +0900

    (08/15)

---
+++ src/Ex07.java
@@ -0,0 +1,15 @@
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.SystemMenuBar;
+
+public class Ex07 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int value = (int)(Math.random()*6)+1;
+		
+		System.out.println("value:"+value);
+		
+				
+		
+	}
+
+}
